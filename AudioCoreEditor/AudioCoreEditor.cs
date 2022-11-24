da028dc5eb0456d446605f961dc084696e11f2c0
Falkor <tobaksduo@gmail.com>
2022-11-24 16:43:01 +0100

    Automatically assign import settings to editor

--- AudioCoreEditor/AudioCoreEditor.cs
+++ AudioCoreEditor/AudioCoreEditor.cs
@@ -11,18 +11,24 @@ namespace AudioCoreLib
             if (SessionState.GetBool("AudioCoreEditorInitialized", false))
                 return;
 
+            SessionState.SetBool("AudioCoreEditorInitialized", true);
+
             string[] res = System.IO.Directory.GetFiles(Application.dataPath, "AudioCoreEditor.dll", System.IO.SearchOption.AllDirectories);
             if (res.Length == 0)
                 return;
 
             string path = res[0].Replace("\\", "/");
-            path = path.Substring(path.IndexOf("/Assets/") + 1);
+            path = path[(path.IndexOf("/Assets/") + 1)..];
 
 
             PluginImporter importer = AssetImporter.GetAtPath(path) as PluginImporter;
-            importer.SetCompatibleWithPlatform(EditorUserBuildSettings.activeBuildTarget, false);
-
-            SessionState.SetBool("AudioCoreEditorInitialized", true);
+            if (importer.GetCompatibleWithPlatform(EditorUserBuildSettings.activeBuildTarget) || importer.GetCompatibleWithAnyPlatform())
+            {
+                importer.SetCompatibleWithAnyPlatform(false);
+                importer.SetCompatibleWithPlatform(EditorUserBuildSettings.activeBuildTarget, false);
+                importer.SetCompatibleWithEditor(true);
+                importer.SaveAndReimport();
+            }
         }
     }
 }
